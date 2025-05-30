ea3d29522cab4a967a9674c9a72ac7408f62839a
Yasmin <ycrisostomo051@gmail.com>
2025-05-30 14:34:39 -0300

    ex2

---
+++ ex2.js
@@ -0,0 +1,12 @@
+function trueFalse(numero) {
+    if(numero % 2 == 0) {
+      return true  
+    
+    } else {
+      return false
+ }
+}
+console.log(trueFalse(4));
+console.log(trueFalse(17));
+console.log(trueFalse(0));
+console.log(trueFalse(-8));
